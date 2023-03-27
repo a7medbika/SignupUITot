f76878735f7e180cb077a714f48ecd0234c49588
Ahmed Bika <74166118+a7medbika@users.noreply.github.com>
2023-03-27 11:46:25 +0200

    Add project files.

---
+++ SignupUITot/UserControls/MyOption.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignupUITot.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MyOption.xaml
+    /// </summary>
+    public partial class MyOption : UserControl
+    {
+
+
+
+        public string text
+        {
+            get { return (string)GetValue(textProperty); }
+            set { SetValue(textProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty textProperty =
+            DependencyProperty.Register("text", typeof(string), typeof(MyOption));
+
+
+        public FontAwesome.WPF.FontAwesomeIcon Icon
+        {
+            get { return (FontAwesome.WPF.FontAwesomeIcon)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(FontAwesome.WPF.FontAwesomeIcon), typeof(MyOption));
+
+
+
+        public MyOption()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SignupUITot/UserControls/MyTextBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignupUITot.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MyTextBox.xaml
+    /// </summary>
+    public partial class MyTextBox : UserControl
+    {
+
+
+
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for HintProp.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HintProperty =
+            DependencyProperty.Register("Hint", typeof(string), typeof(MyTextBox));
+
+
+
+        public MyTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
